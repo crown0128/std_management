351bf0bb7e44b2d2f9186e403f52e9369fec31e2
isaque <happycrown0128@gmail.com>
2023-10-28 04:51:44 -0700

    std_form center tailwind css

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -4,11 +4,11 @@ import Register from "../Components/register";
 const App = () => {
 
     return (
-        <div>
+        <div className="relative text-center">
             <h1 className="font-bold text-center text-2xl">
                 Student Management System
             </h1>
-            <div className="w-1/2">
+            <div className="absolute right-0 left-0 mx-auto w-1/2 text-center">
                 <Register />
             </div>
         </div>
